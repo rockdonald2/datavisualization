d21afd3fbecaef2e1c4ac2761d0602298e3526e8
rockdonald2 <rockdonald2@gmail.com>
2020-07-03 15:27:13 +0300

    2020.07.03

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
+    container: {
+      center: true
+    },
     extend: {},
   },
-  variants: {},
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    transform: ['responsive', 'hover', 'focus', 'group-hover'],
+    translate: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
   plugins: [],
 }
