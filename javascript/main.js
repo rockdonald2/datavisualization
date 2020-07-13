7fbc02334571a043879fbdc4b62c6a7a6d14e209
rockdonald2 <rockdonald2@gmail.com>
2020-07-13 14:46:06 +0300

    2020.07.13

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 hljs.initHighlightingOnLoad();
 
 const modified = document.querySelector('#modified') ? document.querySelector('#modified') : null;
-const lastModified = '2020 Július 12.'
+const lastModified = '2020 Július 13.'
 
 modified != null ? modified.textContent = 'Utolsó modósítás: ' + lastModified : null;
 
